4e33ad1d1e15f7e709af9037882a71e38680e982
muhyusup23 <muhammadyusup23smi@gmail.com>
2023-10-30 12:58:59 +0700

    About page
    About page

--- src/Pages/AboutPage/AboutPage.js
+++ src/Pages/AboutPage/AboutPage.js
@@ -1,10 +1,17 @@
-import About from '../../Components/AboutComp/AboutComp';
-import Navigation from '../../Components/Navigation/Navigation';
+import Navigation from "../../Components/Navigation/Navigation";
+import About from "../../Components/AboutComp/AboutComp";
+import Values from "../../Components/AboutComp/Values";
+import VisiMisi from "../../Components/AboutComp/Visimisi";
+import Footer from "../../Components/Footer/Footer";
+
 const AboutPage = () => {
   return (
     <>
       <Navigation />
       <About />
+      <Values />
+      <VisiMisi />
+      <Footer />
     </>
   );
 };
